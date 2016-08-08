8a3368b283ff256c92d7d48ee8ae5ffb26e15c2c
DienTranVan <tranvandien.k55@hus.edu.vn>
2016-08-08 17:20:21 +0700

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,6 +4,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -14,5 +26,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
